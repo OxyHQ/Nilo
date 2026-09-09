b79f13afbcda81645f5cd5d4d43d275f93d3e5be
Nate Isern <nate.isern.alvarez@gmail.com>
2026-09-10 01:40:19 +0300

    chore: update Oxy SDK edge telemetry

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -91,7 +91,7 @@ app.use((req, res, next) => {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'X-Device-Info', 'X-Workspace-Id'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'X-Device-Info', 'X-Workspace-Id', 'X-Oxy-Edge-Region'],
     optionsSuccessStatus: 200,
   })(req, res, next);
 });
